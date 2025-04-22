cb55cf96858c7b90d541c553395ff192938da731
Rashib Khanal <contact@rashib.dev>
2025-04-22 11:19:23 -0500

    update: react instead of vanilla js, gonnna be easy to make multiple pages web extension

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+  <>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure beatae magnam voluptatum laudantium blanditiis numquam nisi saepe, vero aliquam aperiam! Vero velit tenetur eveniet.</p>
+  
+    </>
+  )
+}
+
+export default App
