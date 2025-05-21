814aba3e2484549f0ef4bcbf310547168fcd1a8f
Rashib Khanal <contact@rashib.dev>
2025-05-21 15:59:07 -0500

    add: UI for NavBar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import { ChevronLeft } from 'lucide-react';
+import { UserRoundCog } from 'lucide-react';
 import { useSelector } from 'react-redux'
 import Settings from '../pages/Settings'
+import { Link } from 'react-router-dom'
 
 function NavBar() {
     const userData = useSelector((state) => (state.user.userData))
     console.log('userData from navbar:', userData)
   return (
     <div>
-      {userData && <Settings />}
+      {/* {userData && <Settings />} */}
+          <nav className='navbar'>
+          <ul>
+      <li>
+          <ChevronLeft  width={32} height={32} color='white'
+          className='left-button'/>
+      </li>
+      <li>
+        <Link to={<Settings />} ><UserRoundCog  width={32} height={32} color='white' className='user-settings' /></Link> 
+      </li>
+  </ul>
+  
+</nav>
+
     </div>
   )
 }
