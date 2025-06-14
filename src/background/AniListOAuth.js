ba04476ec77be4e3d33dc07f951ca42d1026f1a5
Rashib Khanal <contact@rashib.dev>
2025-06-14 00:23:57 -0500

    add: function to get code from redirect url

--- src/background/AniListOAuth.js
+++ src/background/AniListOAuth.js
@@ -2,18 +2,29 @@ export const ANILIST_CLIENT_ID = "27628";
 export const ANILIST_REDIRECT_URI = browser.identity.getRedirectURL();
 
 export async function AniListAuth() {
-  console.log("I am in start of AniList Auth");
+  const redirectURL = await startAniListAuth();
+  return validate(redirectURL);
+}
+
+export async function startAniListAuth() {
   const response_type = "code";
   let authURL = "https://anilist.co/api/v2/oauth/authorize";
   authURL += `?response_type=${response_type}`;
   authURL += `&client_id=${ANILIST_CLIENT_ID}`;
   authURL += `&redirect_uri=${ANILIST_REDIRECT_URI}`;
 
-  const response = await browser.identity.launchWebAuthFlow({
+  return await browser.identity.launchWebAuthFlow({
     interactive: true,
     url: authURL,
   });
-  console.log(response)
+}
+
+export async function validate(redirectURL) {
+  console.log("I am in validate thing");
+  const params = new URL(redirectURL).searchParams;
+  const code = params.get("code");
+
+  return code;
 }
 
 export async function getAniListUserData(sendResponse) {
--- src/background/MALOAuth.js
+++ src/background/MALOAuth.js
@@ -6,8 +6,9 @@ import {
 export const MAL_CLIENT_ID = "db72b0c4364bb89f8c4bc7991b734bee";
 export const MAL_REDIRECT_URI = browser.identity.getRedirectURL();
 
-export function MALAuth() {
-  return startMALAuth().then(validate);
+export async function MALAuth() {
+  const redirectURL = await startMALAuth();
+  return validate(redirectURL);
 }
 
 export async function startMALAuth() {
