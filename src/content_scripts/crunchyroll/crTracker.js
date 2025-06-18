1914c640f9979962c19ee4545424c65a625311dd
Rashib Khanal <contact@rashib.dev>
2025-06-18 10:57:48 -0500

    add: function to wait for element to load before accessing DOM

--- src/content_scripts/crunchyroll/crTracker.js
+++ src/content_scripts/crunchyroll/crTracker.js
@@ -1,7 +1,41 @@
+import { time } from "motion";
 import { watchRouteChanges } from "../watchRouteChange";
-
-console.log("crTracker loaded");
 
 watchRouteChanges((newUrl) => {
   console.log("new url:", newUrl);
 });
+
+// when page is loaded
+
+if (url.pathname.includes("/watch/")) {
+  console.log("user is on the watch page!!");
+}
+
+async function startCRTracker() {
+  const url = new URL(window.location.href);
+  if (!url.pathname.includes("/watch/")) return;
+
+  console.log("User is on watch page");
+  const info = await extractAnimeInfoFromCR();
+}
+
+async function extractAnimeInfoFromCR() {}
+
+async function waitForElementtoLoad(selector, timeout = 5000) {
+  return new Promise((resolve, reject) => {
+    // automatically rejects the promise after 5000 ms
+    const timer = setTimeout(() => {
+      reject(new Error(`Element not found: ${selector}`));
+    }, timeout);
+
+    // check the DOM every 100ms to see if the element is available
+    const interval = setInterval(() => {
+      const el = document.querySelector(selector);
+      if (el) {
+        clearInterval(interval); // stop setInterval
+        clearTimeout(timer); // cancel the timer
+        resolve(el); // sends resolve
+      }
+    }, 100);
+  });
+}
